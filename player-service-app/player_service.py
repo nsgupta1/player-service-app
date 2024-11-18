6d8a5773055cd7ab2dabfc4170e1a0a58438df67
Eric Fakhourian <ericfakhourian@Erics-MacBook-Pro.local>
2024-11-18 12:28:46 -0800

    feature2 added code to return columns as part of response

--- player-service-app/player_service.py
+++ player-service-app/player_service.py
@@ -6,21 +6,27 @@ class PlayerService:
         conn = sqlite3.connect("player.db")
         self.conn = conn
         self.cursor = conn.cursor()
+        self.columns = self.get_columns()
 
     def get_all_players(self):
 
         query = "SELECT * FROM players"
         result = self.cursor.execute(query).fetchall()
+        players = []
+        for row in result:
+            dic = self.convert_row_to_dict(row)
+            players.append(dic)
 
-        return result
+        return players
 
     def search_by_player(self, player_id):
 
         query = "SELECT * FROM players WHERE playerID='{}'".format(player_id)
         result = self.cursor.execute(query).fetchall()
 
-        return result
-
+        for row in result:
+            dic = self.convert_row_to_dict(row)
+        return dic
 
     def search_by_country(self, birth_country):
 
@@ -29,3 +35,13 @@ class PlayerService:
 
         return result
 
+
+    def convert_row_to_dict(self, row):
+        dic = { self.columns[i]: row[i] for i in range(len(row)) }
+        return dic
+
+
+    def get_columns(self):
+        self.cursor.execute("PRAGMA table_info(players)")
+        columns = [column[1] for column in self.cursor.fetchall()]
+        return columns
